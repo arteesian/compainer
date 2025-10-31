f7dec14f29fe90be8e01dbe6646e04a439e3b8f6
Беловолов Михаил <mbelovolov@pari.paricorp.ru>
2025-10-31 10:37:01 +0300

    pls

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def create_app():
     app.add_middleware(
         SessionMiddleware,
         secret_key=settings.SESSION_SECRET_KEY,
-        same_site="none",
+        same_site="lax",
         # max_age=30,
         https_only=True,
     )
